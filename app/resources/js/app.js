8fca2c17c0285f3e6f6debfe103b1d99b09ed24b
Junel Solis <junel@junelsolis.com>
2020-07-17 10:04:58 +0500

    add frontend vue-based scaffold

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
