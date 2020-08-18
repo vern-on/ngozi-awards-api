e3dc272427bd9b9c741650a3b539f5e904a81e96
Junel Solis <junel@junelsolis.com>
2020-08-18 09:56:54 +0500

    add migrations

---
+++ app/database/migrations/2020_07_17_054448_create_category_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_questions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('question_category_id');
+            $table->unsignedBigInteger('question_id');
+            $table->timestamps();
+
+            $table->foreign('question_category_id')->references('id')->on('question_categories')->onDelete('cascade');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_questions');
+    }
+}
