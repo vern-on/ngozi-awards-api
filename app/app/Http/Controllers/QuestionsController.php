801dcc1a87d4ddb0e7feceb67346c54eef2e7e5e
Junel Solis <junel@junelsolis.com>
2020-08-18 09:53:49 +0500

    add models and controllers

---
+++ app/app/Http/Controllers/QuestionsController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Question;
+use App\QuestionCategory;
+
+class QuestionsController extends Controller
+{
+    public function get(Request $request)
+    {
+        if (!$request->expectsJson()) {
+            return view('faq');
+        }
+
+        $query = Question::query()->with('categories');
+
+        if ($request->filled('filter')) {
+            $query = $query->where(function ($query) use ($request) {
+                return $query->where('title', 'like', '%' . $request['filter'] . '%')
+                    ->orWhere('slug', 'like', '%' . $request['filter'] . '%')
+                    ->orWhere('content', 'like', '%' . $request['filter'] . '%');
+            });
+        }
+
+        if ($request->filled('category')) {
+            $query = $query->whereHas('categories', function ($query) use ($request) {
+                $query->where('slug', '=', $request['category']);
+            });
+        }
+
+        return response()->json(['questions' => $query->get()]);
+    }
+
+    public function categories()
+    {
+        $categories = QuestionCategory::get();
+
+        return response()->json(['categories' => $categories]);
+    }
+
+    public function popular()
+    {
+        $questions = Question::orderByDesc('read_count')->take(6)->get();
+
+        return response()->json(['popular_questions' => $questions]);
+    }
+
+    public function increment(Request $request)
+    {
+        $question = Question::where('slug', $request['slug'])->firstOrFail();
+        $question->update(['read_count' => $question->read_count + 1]);
+
+        return response()->json();
+    }
+}
---
+++ app/app/Http/Controllers/SponsorsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Sponsor;
+
+class SponsorsController extends Controller
+{
+    public function partners()
+    {
+        if (!request()->expectsJson()) {
+            return view('partners');
+        }
+
+        $partners = Sponsor::paginate(12);
+
+        return response()->json(['partners' => $partners]);
+    }
+}
